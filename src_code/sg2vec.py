ff6c9ac2eb96bbfac87d0f9ba70d14350dc794a9
root <root@annamalai-Lenovo-Y50-70.(none)>
2017-04-21 02:07:58 +0530

    realign

--- src_code/sg2vec.py
+++ src_code/sg2vec.py
@@ -27,67 +27,52 @@ def shuffle(sents):
     random.shuffle(sents)
     return sents
 
-if sys.argv[1] == '-h' or sys.argv[1] == '--help':
-    print 'commandline args: <src_dir> <max_num_files> <file_extension> <opfname_prefix> <embedding_dim> <iterations> <# of cpu cores>'
-    exit (0)
 
-src_dir = sys.argv[1]
-max_num_files = int(sys.argv[2])
-extn = sys.argv[3]
-opfname_prefix = sys.argv[4]
-dim = int(sys.argv[5])
-iters = int(sys.argv[6])
-n_cpus = int(sys.argv[7])
+def main (debug=False):
+    wlk_target_contexts_dir = sys.argv[1]
+    max_num_files = int(sys.argv[2])
+    extn = sys.argv[3]
+    opfname_prefix = sys.argv[4]
+    dim = int(sys.argv[5])
+    iters = int(sys.argv[6])
+    n_cpus = int(sys.argv[7])
 
-#src_dir = '../tmp/MySentencesWithOutDWL1/DrebinWL1/'
-#num_files = 1000
-#extn = 'Removed'
-#opfname_prefix = 'subgraph2vec'
-#dim = 32
-#iters = 10
-#n_cpus = 20
+    #src_dir = '../tmp/MySentencesWithOutDWL1/DrebinWL1/'
+    #num_files = 1000
+    #extn = 'Removed'
+    #opfname_prefix = 'subgraph2vec'
+    #dim = 32
+    #iters = 10
+    #n_cpus = 20
 
-sentences = list(MySentences(src_dir, max_num_files, extn))
 
-neg = 20 #num of negative samples for skipgram model
-sg = 1
+    sentences = list(MySentences(wlk_target_contexts_dir, max_num_files, extn))
 
-model = Word2Vec(sentences, min_count=1,
-                 size=dim,
-                 sg=sg,
-                 negative=neg,
-                 workers=n_cpus,
-                 iter=iters)
+    neg = 20 #num of negative samples for skipgram model
+    sg = 1
 
-# for i in xrange(2):
-#     sentences = shuffle(sentences)
-#     model.train(sentences=sentences)
+    model = Word2Vec(sentences, min_count=1,
+                     size=dim,
+                     sg=sg,
+                     negative=neg,
+                     workers=n_cpus,
+                     iter=iters)
 
-# '''
-TestApis = ['Landroid/telephony/gsm/SmsManager;->getDefault',
-            'Landroid/content/Context;->getContentResolver',
-            'Ljava/lang/Class;->forName',
-            'Landroid/telephony/TelephonyManager;->getSimCountryIso',
-            'Landroid/telephony/gsm/SmsManager;->sendMultipartTextMessage',
-            'Landroid/content/ContentResolver;->update',
-            'Ljava/lang/reflect/Method;->invoke',
-            'Landroid/location/Location;->getLongitude',
-            'Ljava/net/URLConnection;->getOutputStream#Ljava/net/URLConnection;->setRequestProperty',
-            'Landroid/os/Handler;->sendMessage#Landroid/content/Context;->bindService',
-            'Landroid/content/Intent;->getParcelableExtra#Landroid/app/Activity;->getIntent']
+    if debug:
+        vocab = model.vocab.keys()
+        for w in vocab[:10]:
+            print 'closest subgraphs to: {} are as follows'.format(w)
+            print '-' * 80
+            pprint(model.most_similar(w))
+            print '-' * 80
 
-for A in TestApis:
-    print A
-    try:
-        pprint (model.most_similar(A))
-    except:
-        print 'not found'
-        continue
-    print '-'*80
-# '''
-vocab = model.vocab.keys()
-for w in vocab[:10]:
-    print w
-    pprint(model.most_similar(w))
-    print '-' * 80
-model.save(opfname_prefix + '_' + '_'.join([str(max_num_files), str(dim), str(sg), str(neg), str(iters)]))
+    opfname = os.path.join ('../models', opfname_prefix + '_' + '_'.join([str(max_num_files), str(dim), str(sg), str(neg), str(iters)]))
+    model.save(opfname)
+
+    print 'output gensim model containing all the subgraphs and their {} ' \
+          'dimensional emeddings for the files from folder {} is saved at {}'\
+        .format(dim, wlk_target_contexts_dir, opfname)
+
+
+if __name__ == '__main__':
+    main (debug=False)
