a6925383eb1968c458856a7f5387e1825e80f9c6
root <root@annamalai-Lenovo-Y50-70.(none)>
2017-04-21 01:04:58 +0530

    adding init script

---
+++ init.sh
@@ -0,0 +1,14 @@
+mkdir models
+mkdir data
+echo "created models and data folder (pls check by typing ls later)"
+
+mv src_code/Annaword2vec.py /usr/local/lib/python2.7/dist-packages/gensim/models/Annaword2vec.py
+mv src_code/mod_word2vec_inner.so /usr/local/lib/python2.7/dist-packages/gensim/models/mod_word2vec_inner.so
+echo "moved Annaword2vec.py and mod_word2vec_inner.so (containing code for radial skip-gram) to /usr/local/lib/python2.7/dist-packages/gensim/models"
+
+cd /usr/local/lib/python2.7/dist-packages/gensim/models/
+mv word2vec_inner.so org_word2vec_inner.so
+echo "renamed the word2vec_inner.so at /usr/local/lib/python2.7/dist-packages/gensim/models to org_word2vec_inner.so"
+ln -s mod_word2vec_inner.so word2vec_inner.so
+echo "created softlink to mod_word2vec_inner.so at /usr/local/lib/python2.7/dist-packages/gensim/models - here onwards, word2vec will use \"radial skipgram\""
+cd -
