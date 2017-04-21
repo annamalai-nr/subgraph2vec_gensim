8f8b1ec431eed41f9906091a5f555ef48c1085d2
root <root@annamalai-Lenovo-Y50-70.(none)>
2017-04-21 12:55:17 +0530

    added code gensim version migration - version used for KDD MLG 2016 paper is gensim 0.12.4

--- init.sh
+++ init.sh
@@ -5,7 +5,12 @@ echo "created models and data folder"
 tar -xvzf kdd_datasets.tar.gz
 excho "untared the dataset provided in the \"Deep Graph Kernels\" paper by Pinar Yanardag and SVN Vishwanathan"
 
-mv src_code/Annaword2vec.py /usr/local/lib/python2.7/dist-packages/gensim/models/Annaword2vec.py
+pip uninstall gensim
+echo "removed existing gensim - if needed, pls re-install using \"pip install -U gensim\""
+pip install gensim==0.12.4
+echo "installed gensim version 0.12.4 - the version used for experiments in our MLG 2016 paper"
+
+mv src_code/Annaword2vec.py /usr/local/lib/python2.7/dist-packages/gensim/models/Annaword2vec.py #contain code for accessing radial skipgram through word2vec api
 mv src_code/mod_word2vec_inner.so /usr/local/lib/python2.7/dist-packages/gensim/models/mod_word2vec_inner.so
 echo "moved Annaword2vec.py and mod_word2vec_inner.so (containing code for radial skip-gram) to /usr/local/lib/python2.7/dist-packages/gensim/models"
 
